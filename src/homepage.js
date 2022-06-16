0a3110feca9b654898568754a034ef2938d896dd
trentkeyes <tkeyes@gmail.com>
2022-06-15 22:50:21 -0500

    Add css file and basic styling, create basic nav bar.

--- src/homepage.js
+++ src/homepage.js
@@ -1,9 +1,53 @@
 const homepage = () => {
   const content = document.querySelector("#content");
 
-  const headline = document.createElement("h1");
-  headline.textContent = "The Second Best Pizza Restaurant in this city";
-  content.appendChild(headline);
+  const header = document.createElement("header");
+  header.setAttribute("class", "header");
+  content.appendChild(header);
+
+  const nav = document.createElement("nav");
+  nav.setAttribute("class", "nav");
+  header.appendChild(nav);
+
+  const ul = document.createElement("ul");
+  ul.setAttribute("class", "ul");
+  nav.appendChild(ul);
+
+  const link1 = document.createElement("li");
+  link1.setAttribute("class", "nav-link");
+  ul.appendChild(link1);
+
+  const a1 = document.createElement("a");
+  link1.appendChild(a1);
+  a1.setAttribute("href", "#");
+  a1.textContent = "Home";
+
+  const link2 = document.createElement("li");
+  link2.setAttribute("class", "nav-link");
+  ul.appendChild(link2);
+
+  const a2 = document.createElement("a");
+  link2.appendChild(a2);
+  a2.setAttribute("href", "#");
+  a2.textContent = "Contact";
+
+  const link3 = document.createElement("li");
+  link3.setAttribute("class", "nav-link");
+  ul.appendChild(link3);
+
+  const a3 = document.createElement("a");
+  link3.appendChild(a3);
+  a3.setAttribute("href", "#");
+  a3.textContent = "Menu";
+
+  const title = document.createElement("h1");
+  title.textContent = "The Second Best Pizza Restaurant in this city";
+  content.appendChild(title);
+  title.setAttribute("class", "title");
+
+  const pizzaPic = document.createElement("img");
+  pizzaPic.setAttribute("src", "/src/pizza.jpeg");
+  content.appendChild(pizzaPic);
 
   const welcomeCopy = document.createElement("p");
   welcomeCopy.textContent =
